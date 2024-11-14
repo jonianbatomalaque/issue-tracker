82165022080125a88778d79b776d9fa7cfbb19ef
Jon-Ian A. Batomalaque <jonab@stpeter.com.ph>
2024-11-14 09:09:19 +0800

    remove homepage

--- app/page.tsx
+++ app/page.tsx
@@ -4,27 +4,11 @@ import LatestIssues from "./LatestIssues";
 import IssueSummary from "./IssueSummary";
 import prisma from "@/prisma/client";
 import IssueChart from "./IssueChart";
-import { Flex, Grid } from "@radix-ui/themes";
+import { Flex, Grid, Text } from "@radix-ui/themes";
 import { Metadata } from "next";
 
 export default async function Home() {
-  const open = await prisma.issue.count({ where: { status: "OPEN" } });
-  const inProgress = await prisma.issue.count({
-    where: { status: "IN_PROGRESS" },
-  });
-  const closed = await prisma.issue.count({ where: { status: "CLOSED" } });
-
-  return (
-    <Grid columns={{ initial: "1", md: "2" }} gap="5">
-      <Flex direction="column" gap="5">
-        <IssueSummary open={open} inProgress={inProgress} closed={closed} />
-        <IssueChart open={open} inProgress={inProgress} closed={closed} />
-      </Flex>
-      <Flex>
-        <LatestIssues />
-      </Flex>
-    </Grid>
-  );
+  return <Text>Hello World</Text>;
 }
 export const metadata: Metadata = {
   title: "Issue Tracket - Dashboard",
